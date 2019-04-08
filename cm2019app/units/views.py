e844f5bb9040d72384eb765a4b33979c3f59e1eb
Josef Wechselauer <wechselauer.josef@gmx.at>
2019-04-08 16:29:36 +0200

    parse weight, show correct image

--- cm2019app/units/views.py
+++ cm2019app/units/views.py
@@ -17,4 +17,17 @@ def index(request):
     context= {
         'data_loaded': data_loaded
     }
+
+    weight =int(splitUnits(data_loaded.get('weight'))[0])
+
+    if weight <= 10:
+        data_loaded['image'] = data_loaded.get('image_light')
+    elif weight <= 100:
+        data_loaded['image'] = data_loaded.get('image_middle')
+    else:
+        data_loaded['image'] = data_loaded.get('image_heavy')
+
     return render(request, "units/index.html", context)
+
+def splitUnits(givenString):
+    return givenString.split(" ",  1)
